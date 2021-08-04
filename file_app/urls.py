47525eafbeaa4bb2720bf5673097c91020183a17
fakhry.trifi1 <fakhry.trifi1@planetgroupint.com>
2021-08-04 14:06:51 +0100

    upload download files

---
+++ file_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import FileView
+
+urlpatterns = [
+    path('upload/', FileView.as_view(), name='file-upload'),
+    path('download/', FileView.as_view(), name='download'),
+]
